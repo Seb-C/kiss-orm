c594b80912de576276cc2a8a39437852bb68270c
Sébastien CAPARROS <sebastiencaparros@gmail.com>
2020-09-03 17:23:28 +0900

    Creating the sequence method

--- src/Databases/DatabaseInterface.ts
+++ src/Databases/DatabaseInterface.ts
@@ -4,4 +4,16 @@ export default interface DatabaseInterface {
 	disconnect(): Promise<void>;
 	query(query: SqlQuery): Promise<any[]>;
 	migrate(migrations: { [key: string]: SqlQuery }): Promise<void>;
+
+	/**
+	 * The sequence function allows to perform several queries,
+	 * sequentially and on a dedicated connection. This is especially useful
+	 * to ensure that multi-queries transactions are done on the same connection,
+	 * and without any other async query interfering.
+	 */
+	sequence<T>(
+		sequence: (
+			query: (query: SqlQuery) => Promise<any[]>,
+		) => Promise<T>,
+	): Promise<T>;
 };
